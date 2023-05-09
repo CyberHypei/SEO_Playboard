cdf12110db1d8193c24a491b2c24579a66b1648d
CyberHypei <gauravbhardwaj.it23@jecrc.ac.in4>
2023-05-09 23:06:39 +0530

    Adding Split frame component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,19 @@ import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+const demos = {
+  soundcloud:
+    '<iframe width="100%" height="700" scrolling="yes" frameborder="yes" src="https://www.flipkart.com/" header></iframe>'
+};
+
+function Iframe(props) {
+  return (
+    <div
+      dangerouslySetInnerHTML={{ __html: props.iframe ? props.iframe : "" }}
+    />
+  );
+}
+
 function App() {
   return (
     <>
@@ -11,7 +24,10 @@ function App() {
           <Route path='/' exact/>
         </Switch>
         <h1>Welcome Aboard!</h1>
-        <h2> Ule Ulo </h2>
+        <div className="Split_frame">
+          <h1>Split Window to inspect product</h1>
+          <Iframe iframe={demos["soundcloud"]} />,
+        </div>
       </Router>
       
     </>
